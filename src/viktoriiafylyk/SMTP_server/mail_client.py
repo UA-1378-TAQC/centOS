912656af71b716ff5260e93af6885a49a18da8a5
viktoriiafylyk <vfilik194@gmail.com>
2025-05-13 15:04:21 +0300

    removed global variables and added small changes

--- src/viktoriiafylyk/SMTP_server/mail_client.py
+++ src/viktoriiafylyk/SMTP_server/mail_client.py
@@ -4,33 +4,37 @@ import imaplib
 import email
 from email.message import EmailMessage
 from dotenv import load_dotenv
+from typing import Dict
 
-load_dotenv()
 
-SMTP_SERVER = os.getenv('SMTP_SERVER')
-SMTP_PORT = int(os.getenv('SMTP_PORT'))
-IMAP_SERVER = os.getenv('IMAP_SERVER')
-IMAP_PORT = int(os.getenv('IMAP_PORT'))
-EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
-EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+def load_config() -> Dict[str, str]:
+    load_dotenv()
+    return {
+        'SMTP_SERVER': os.getenv('SMTP_SERVER'),
+        'SMTP_PORT': int(os.getenv('SMTP_PORT')),
+        'IMAP_SERVER': os.getenv('IMAP_SERVER'),
+        'IMAP_PORT': int(os.getenv('IMAP_PORT')),
+        'EMAIL_ADDRESS': os.getenv('EMAIL_ADDRESS'),
+        'EMAIL_PASSWORD': os.getenv('EMAIL_PASSWORD')
+    }
 
-def send_email(subject, body):
+def send_email(config: Dict[str, str], subject: str, body: str):
     msg = EmailMessage()
-    msg['From'] = EMAIL_ADDRESS
-    msg['To'] = EMAIL_ADDRESS
+    msg['From'] = config['EMAIL_ADDRESS']
+    msg['To'] = config['EMAIL_ADDRESS']
     msg['Subject'] = subject
     msg.set_content(body)
 
-    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+    with smtplib.SMTP(config['SMTP_SERVER'], config['SMTP_PORT']) as server:
         server.send_message(msg)
-    print("Лист надіслано.")
+    print("Mail is sent.")
 
-def read_emails():
-  with imaplib.IMAP4_SSL(IMAP_SERVER, IMAP_PORT) as mail:
-        mail.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+def read_emails(config: Dict[str, str]):
+    with imaplib.IMAP4_SSL(config['IMAP_SERVER'], config['IMAP_PORT']) as mail:
+        mail.login(config['EMAIL_ADDRESS'], config['EMAIL_PASSWORD'])
         mail.select('INBOX')
 
-        status, messages = mail.search(None, f'FROM "{EMAIL_ADDRESS}"')
+        status, messages = mail.search(None, f'FROM "{config["EMAIL_ADDRESS"]}"')
         email_ids = messages[0].split()
 
         for eid in email_ids:
@@ -40,8 +44,9 @@ def read_emails():
                     msg = email.message_from_bytes(response_part[1])
                     subject = msg['subject']
                     from_ = msg['from']
-                    print(f'Від: {from_}\nТема: {subject}\n')
+                    print(f'From: {from_}\nSubject: {subject}\n')
 
 if __name__ == "__main__":
-    send_email("Test mail2", "This is second test mail, sent from Python.")
-    read_emails()
+    config = load_config()
+    send_email(config, "Test mail", "This is test mail, sent from Python.")
+    read_emails(config)

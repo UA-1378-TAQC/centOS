1db7179805a5a049161e3a3f9e658840aef659af
viktoriiafylyk <vfilik194@gmail.com>
2025-05-13 15:25:51 +0300

    removed globals

--- src/viktoriiafylyk/copy_file/script_to_copy.py
+++ src/viktoriiafylyk/copy_file/script_to_copy.py
@@ -2,24 +2,30 @@ import paramiko
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+def load_ssh_config():
+    load_dotenv()
+    return {
+        'host': os.getenv('SSH_HOST'),
+        'port': int(os.getenv('SSH_PORT')),
+        'username': os.getenv('SSH_USER'),
+        'password': os.getenv('SSH_PASS')
+    }
 
-host = os.getenv('SSH_HOST')  
-port = 22
-username = os.getenv('SSH_USER')  
-password = os.getenv('SSH_PASS')     
-local_path = 'CentOS_Commands.txt'
-remote_path = '/home/viktoriiafylyk/course_files/CentOS_Commands.txt'  
+def transfer_file(ssh_config, local_path, remote_path):
+    try:
+        transport = paramiko.Transport((ssh_config['host'], ssh_config['port']))
+        transport.connect(username=ssh_config['username'], password=ssh_config['password'])
 
-# Підключення та передача файлу
-try:
-    transport = paramiko.Transport((host, port))
-    transport.connect(username=username, password=password)
+        sftp = paramiko.SFTPClient.from_transport(transport)
+        sftp.put(local_path, remote_path)
+        sftp.close()
+        transport.close()
+        print("Файл успішно передано.")
+    except Exception as e:
+        print(f"Помилка при передачі файлу: {e}")
 
-    sftp = paramiko.SFTPClient.from_transport(transport)
-    sftp.put(local_path, remote_path)
-    sftp.close()
-    transport.close()
-    print("Файл успішно передано.")
-except Exception as e:
-    print(f"Помилка при передачі файлу: {e}")
+if __name__ == "__main__":
+    ssh_config = load_ssh_config()
+    local_file = 'CentOS_Commands.txt'
+    remote_file = '/home/viktoriiafylyk/course_files/CentOS_Commands.txt'
+    transfer_file(ssh_config, local_file, remote_file)
